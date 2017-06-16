0804161fee72f6f6f9e70fce8a4025865c1c4e55
Raja-Raman <rajavaidya@gmail.com>
2017-06-16 15:28:54 +0530

    Update and rename ultra1.ino.txt to ultra1.ino

--- ultra1/ultra1.ino
+++ ultra1/ultra1.ino
@@ -31,7 +31,7 @@ void loop() {
 	// Sets the trigPin on HIGH state for 10 micro seconds
 	digitalWrite(trigPin, HIGH);
 	delayMicroseconds(10);
-	digitalWrite(trigPin, LOW);
+	digitalWrite(trigPin, LOW); 
 
 	// Reads the echoPin, returns the sound wave travel time in microseconds
 	duration = pulseIn(echoPin, HIGH);
